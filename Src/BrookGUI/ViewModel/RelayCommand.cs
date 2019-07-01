2e9a6be8f8f7f8826c50f93068bd8adb7dbd91b6
ASquigglyTwist <9925933+asquigglytwist@users.noreply.github.com>
2019-07-01 21:17:07 +0530

    Code for RelayCommand.

---
+++ Src/BrookGUI/ViewModel/RelayCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Brook.MainWin.ViewModel
+{
+    // [BIB]:  https://stackoverflow.com/questions/1468791/icommand-mvvm-implementation
+    public class RelayCommand : ICommand
+    {
+        private Predicate<object> _canExecute;
+        private Action<object> _execute;
+
+        public RelayCommand(Predicate<object> canExecute, Action<object> execute)
+        {
+            _canExecute = canExecute;
+            _execute = execute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
