1619c9f79857e157107326ab52f3e512e8dfaf84
ASquigglyTwist <9925933+asquigglytwist@users.noreply.github.com>
2019-07-04 08:01:01 +0530

    Adding Constants to use in static binding.

---
+++ Src/BrookGUI/Common/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brook.MainWin.Common
+{
+    public static class Constants
+    {
+        public static double DecodePixelHeight => 120;
+        public static double DecodePixelWidth => 120;
+    }
+}
