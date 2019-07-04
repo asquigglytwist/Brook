0f25332e9dfde5de9997429f16b11d1fc39ccbe1
ASquigglyTwist <9925933+asquigglytwist@users.noreply.github.com>
2019-07-04 08:00:17 +0530

    Adding PerfTracker for tracking app performance.

---
+++ Src/BrookGUI/Common/PerfTracker.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brook.MainWin.Common
+{
+    public static class PerfTracker
+    {
+        static Dictionary<string, Stopwatch> allPerfTrackers;
+
+        static PerfTracker()
+        {
+            allPerfTrackers = new Dictionary<string, Stopwatch>();
+        }
+
+        public static void StartTracking(string perfMetricName)
+        {
+            Stopwatch existingStopWatch;
+            if (allPerfTrackers.TryGetValue(perfMetricName, out existingStopWatch))
+            {
+                existingStopWatch.Reset();
+            }
+            else
+            {
+                existingStopWatch = new Stopwatch();
+                allPerfTrackers.Add(perfMetricName, existingStopWatch);
+            }
+            existingStopWatch.Start();
+        }
+
+        public static TimeSpan StopTracking(string perfMetricName)
+        {
+            Stopwatch existingStopWatch;
+            if (allPerfTrackers.TryGetValue(perfMetricName, out existingStopWatch))
+            {
+                existingStopWatch.Stop();
+            }
+            else
+            {
+                throw new ArgumentException($"No performance tracker found for {perfMetricName}");
+            }
+            return existingStopWatch.Elapsed;
+        }
+    }
+}
