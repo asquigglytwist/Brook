59df8c7290fc12b42ad8ccf30b56e1987bc05bd8
ASquigglyTwist <9925933+asquigglytwist@users.noreply.github.com>
2019-07-04 08:03:43 +0530

    Tracking performance using PerfTracker (for reading / parsing huge number of files). Also handling CorruptFileException thrown by TagLib.

--- Src/BrookGUI/ViewModel/DetailsViewModel.cs
+++ Src/BrookGUI/ViewModel/DetailsViewModel.cs
@@ -13,26 +13,44 @@ namespace Brook.MainWin.ViewModel
     public class DetailsViewModel : ViewModelBase
     {
         string _rootDirPath;
+        readonly int totalMp3Files;
+        int processedMp3Files, corruptedMp3Files;
 
         public ObservableCollection<SoundFile> AllSoundFiles
         { get; private set; }
 
         public DetailsViewModel(string rootDirPath)
         {
+            Common.PerfTracker.StartTracking("InitDVM");
             _rootDirPath = rootDirPath;
-            _Init();
+            totalMp3Files = _Init();
+            var elapsedTime = Common.PerfTracker.StopTracking("InitDVM");
+            Logger.Instance.Debug($"Total time elapsed in reading {AllSoundFiles.Count} files:  {elapsedTime}");
+            Logger.Instance.Info($"\nTotal files:  {totalMp3Files}\nProcessed:  {processedMp3Files}\nCorrupted:  {corruptedMp3Files}");
         }
 
-        private void _Init()
+        private int _Init()
         {
             AllSoundFiles = new ObservableCollection<SoundFile>();
             var mp3Files = Helpers.GetFilesWithExtension(_rootDirPath, "mp3");
+            processedMp3Files = corruptedMp3Files = 0;
             for (int i = 0; i < mp3Files.Length; i++)
             {
-                var sFile = new SoundFile(mp3Files[i]);
-                sFile.Init();
-                AllSoundFiles.Add(sFile);
+                try
+                {
+                    var sFile = new SoundFile(mp3Files[i]);
+                    sFile.Init();
+                    AllSoundFiles.Add(sFile);
+                    processedMp3Files++;
+                }
+                catch (TagLib.CorruptFileException cfe)
+                {
+                    Logger.Instance.Error($"CorruptFileException occured with file # {i:00000}:{Environment.NewLine}{mp3Files[i]}");
+                    Logger.Instance.Debug(cfe.AsLoggableString());
+                    corruptedMp3Files++;
+                }
             }
+            return mp3Files.Length;
         }
 
         #region LSVSelectedItemChanged
@@ -63,7 +81,7 @@ namespace Brook.MainWin.ViewModel
 
         public void HandleLSVSelectionChanged(object p)
         {
-            SelectedItemAlbumArtData = SelectedItem.AlbumArtData;
+            //SelectedItemAlbumArtData = SelectedItem.AlbumArtData;
         }
         #endregion
     }
