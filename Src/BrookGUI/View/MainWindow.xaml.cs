22343cfe53ed79232c741ade93c94d47c0137f3c
ASquigglyTwist <9925933+asquigglytwist@users.noreply.github.com>
2019-07-05 09:40:32 +0530

    Code-behind for sorting and filtering.

--- Src/BrookGUI/View/MainWindow.xaml.cs
+++ Src/BrookGUI/View/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Brook.MainWin.ViewModel;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +23,8 @@ namespace Brook.MainWin
     public partial class MainWindow : Window
     {
         DetailsViewModel detailsVM;
+        private GridViewColumnHeader listViewSortCol = null;
+        private SortAdorner listViewSortAdorner = null;
 
         public MainWindow()
         {
@@ -38,6 +41,78 @@ namespace Brook.MainWin
             CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(lsvSoundFiles.ItemsSource);
             PropertyGroupDescription groupDescription = new PropertyGroupDescription("AlbumArtHash");
             view.GroupDescriptions.Add(groupDescription);
+            view.Filter = UserFilter;
+        }
+
+        private bool UserFilter(object item)
+        {
+            if (String.IsNullOrEmpty(txtSearch.Text))
+            {
+                return true;
+            }
+            else
+            {
+                return ((item as Model.SoundFile).Album.IndexOf(txtSearch.Text, StringComparison.OrdinalIgnoreCase) >= 0);
+            }
+        }
+
+        private void lsvSoundFilesColumnHeader_Click(object sender, RoutedEventArgs e)
+        {
+            var column = (sender as GridViewColumnHeader);
+            var sortBy = column.Tag.ToString();
+            if (listViewSortCol != null)
+            {
+                AdornerLayer.GetAdornerLayer(listViewSortCol).Remove(listViewSortAdorner);
+                lsvSoundFiles.Items.SortDescriptions.Clear();
+            }
+            ListSortDirection newDir = ListSortDirection.Ascending;
+            if (listViewSortCol == column && listViewSortAdorner.Direction == newDir)
+            {
+                newDir = ListSortDirection.Descending;
+            }
+            listViewSortCol = column;
+            listViewSortAdorner = new SortAdorner(listViewSortCol, newDir);
+            AdornerLayer.GetAdornerLayer(listViewSortCol).Add(listViewSortAdorner);
+            lsvSoundFiles.Items.SortDescriptions.Add(new SortDescription(sortBy, newDir));
+        }
+
+        private void txtSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CollectionViewSource.GetDefaultView(lsvSoundFiles.ItemsSource).Refresh();
+        }
+    }
+
+    public class SortAdorner : Adorner
+    {
+        private static Geometry ascGeometry = Geometry.Parse("M 0 4 L 3.5 0 L 7 4 Z"),
+            descGeometry = Geometry.Parse("M 0 0 L 3.5 4 L 7 0 Z");
+
+        public ListSortDirection Direction { get; private set; }
+
+        public SortAdorner(UIElement element, ListSortDirection dir)
+            : base(element)
+        {
+            this.Direction = dir;
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+            if (AdornedElement.RenderSize.Width < 20)
+            {
+                return;
+            }
+            TranslateTransform transform = new TranslateTransform(
+                    AdornedElement.RenderSize.Width - 15,
+                    (AdornedElement.RenderSize.Height - 5) / 2);
+            drawingContext.PushTransform(transform);
+            Geometry geometry = ascGeometry;
+            if (this.Direction == ListSortDirection.Descending)
+            {
+                geometry = descGeometry;
+            }
+            drawingContext.DrawGeometry(Brushes.Black, null, geometry);
+            drawingContext.Pop();
         }
     }
 }
